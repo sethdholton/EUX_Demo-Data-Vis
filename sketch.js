06fbd4f782b1b2e8d303b208d861e06fd7fd15de
Seth Holton <sethdholton@utexas.edu>
2024-09-30 10:55:23 -0500

    file upload

--- sketch.js
+++ sketch.js
@@ -1,14 +1,19 @@
 let table;
 
 function preload() {
-    table = loadTable("assets/MovieData.csv");
+    table = loadTable("assets/MovieData.csv", "csv", "header");
 }
 
 function setup() {
     createCanvas(800, 600);
     background(220);
 
-    console()
+    // console.log(table);
+
+    for(let r = 0; r < table.getRowCount(); r++) {
+        const title = table.getString(r, "title");
+        console.log(title);
+    }
 }
 
 function draw() {
